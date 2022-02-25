d19ca50333502c019a5acc3ae32201115d07128b
Evgeniy Kozlov <100347774+evgeniy-kozlov-web@users.noreply.github.com>
2022-02-25 21:20:42 +0200

    Change paths to rulse(path + method)

--- src/routing/Router.php
+++ src/routing/Router.php
@@ -11,52 +11,32 @@ class Router
 		return $this->routes;
 	}
 
-	public function getPaths(): array
+	public function getRoute(string $slug): Route | false
 	{
-		$paths = [];
+		return $this->routes[$slug] ?? false;
+	}
+
+	public function getRouteByRule(string $path, string $method): Route | false
+	{
+		$method = strtoupper($method);
 
 		foreach ($this->routes as $route) {
-			$paths[] = $route->getPath();
+			if ($route->getPath() === $path && $route->getMethod() === $method) return $route;
 		}
 
-		return $paths;
+		return false;
 	}
 
-	public function getRoute(string $slug): Route
+	public function getCurrentRoute(): Route | bool
 	{
-		if (!array_key_exists($slug, $this->routes)) throw new \app\exceptions\SlugIsNotExistsException();
-
-		return $this->routes[$slug];
-	}
-
-	public function getRouteByPath(string $path): Route
-	{
-		foreach ($this->routes as $route) {
-			if ($route->getPath() === $path) return $route;
-		}
-
-		throw new \app\exceptions\PathIsNotExistsException();
-	}
-
-	public function check(): bool
-	{
-		$path = $_SERVER['REQUEST_URI'];
-
-		if (!in_array($path, $this->getPaths())) return false;
-
-		$route = $this->getRouteByPath($path);
-		$method = $route->getMethod();
-
-		return $_SERVER['REQUEST_METHOD'] === $method;
+		return $this->getRouteByRule($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 	}
 
 	public function addRoute(Route $route): Router
 	{
 		if (array_key_exists($route->getSlug(), $this->routes)) throw new \app\exceptions\SlugIsAlreadyExistsException();
 
-		$paths = $this->getPaths();
-
-		if (in_array($route->getPath(), $paths)) throw new \app\exceptions\PathIsAlreadyExistsException();
+		if ($this->getRouteByRule($route->getPath(), $route->getMethod())) throw new \app\exceptions\RuleIsAlreadyExistsException();
 
 		$this->routes[$route->getSlug()] = $route;
 
